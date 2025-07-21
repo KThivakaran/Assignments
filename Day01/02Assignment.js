10bb15bfdc075c261f36ed2cc73b8b75faf73047
KThivakaran <kthivakaran@gmail.com>
2025-07-21 17:25:51 +0530

    Add files via upload
    Day 1 assignments

---
+++ Day01/02Assignment.js
@@ -0,0 +1,32 @@
+let firstName = "Thivakaran";
+let companyName = "NatWest";
+let mobileNumber = 9841811903;
+let isAutiomation = true;
+let hasPlayWright;
+
+console.log("firstName is :"+firstName);
+console.log(typeof firstName);
+
+console.log("Company Name is : "+ companyName);
+console.log(typeof companyName);
+
+console.log("Mobile number is : "+ mobileNumber);
+console.log(typeof mobileNumber);
+
+console.log("isAutomation is : "+ isAutiomation);
+console.log(typeof isAutiomation);
+
+console.log("hasPlayWright is : "+ hasPlayWright);
+console.log(typeof hasPlayWright);
+
+
+console.log(firstName,typeof firstName);
+
+console.log(companyName,typeof companyName);
+
+console.log(mobileNumber,typeof mobileNumber);
+
+console.log(isAutiomation,typeof isAutiomation);
+
+console.log(hasPlayWright,typeof hasPlayWright);
+
---
+++ Day01/FIrstProg.js
@@ -0,0 +1,28 @@
+var MobileNumber = 9841811903;
+var Name = "Thivakaran";
+var accNumber = 9876434567654345676543456765434567654345676543456765433456n;
+var intRate = 6.5;
+var isNumber = true;
+var Data = null
+var testvalue;
+
+console.log("Mobile number is :"+MobileNumber);
+console.log(typeof MobileNumber);
+
+console.log("Name is : "+ Name);
+console.log(typeof Name);
+
+console.log("Acc number is : "+ accNumber);
+console.log(typeof accNumber);
+
+console.log("interest rate is : "+ intRate);
+console.log(typeof intRate);
+
+console.log("isNumber is : "+ isNumber);
+console.log(typeof isNumber);
+
+console.log("Data value is : "+ Data);
+console.log(typeof Data);
+
+console.log("Testvalue is : "+ testvalue);
+console.log(typeof testvalue);
